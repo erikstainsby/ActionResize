712d5d190a6681ff082b4d1b22583f64d90b36c2
Erik Stainsby <erik.stainsby@roaringsky.ca>
2012-02-18 21:09:45 -0800

    stubbed in

---
+++ ActionResize/ActionResize.h
@@ -0,0 +1,13 @@
+//
+//  ActionResize.h
+//  ActionResize
+//
+//  Created by Erik Stainsby on 12-02-18.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import <RSTrixiePlugin/RSTrixiePlugin.h>
+
+@interface ActionResize : RSTrixiePlugin
+
+@end
---
+++ ActionResize/ActionResize.m
@@ -0,0 +1,26 @@
+//
+//  ActionResize.m
+//  ActionResize
+//
+//  Created by Erik Stainsby on 12-02-18.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import "ActionResize.h"
+
+@implementation ActionResize
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+	
+    if( nil != (self = [super initWithNibName:nibNameOrNil	bundle:nibBundleOrNil]))
+    {
+		[self setName: @"Resize"];
+    }
+    return self;
+}
+
+- (BOOL) hasSelectorField {
+	return NO;
+}
+
+@end
